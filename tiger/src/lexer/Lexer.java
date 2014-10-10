c20db242a55954a47786ab40f5cc6b60c8b8f078
seerliu <1921800440@qq.com>
2014-10-10 23:57:37 +0800

    creat tiger
    The tiger compiler v1.0
    accomplished the lexer
    and need to deal with the parser

---
+++ tiger/src/lexer/Lexer.java
@@ -0,0 +1,237 @@
+package lexer;
+
+import static control.Control.ConLexer.dump;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
+
+import lexer.Token.Kind;
+
+public class Lexer
+{
+  String fname; // the input file name to be compiled
+  PushbackInputStream fstream; // input stream for the above file
+  
+  public String s = "";
+  int linenum=1;
+  Token behind=null;
+  Token cmp=null;
+  
+
+  public Lexer(String fname, PushbackInputStream fstream)
+  {
+    this.fname = fname;
+    this.fstream = fstream;
+    
+  }
+  
+
+  // When called, return the next token (refer to the code "Token.java")
+  // from the input stream.
+  // Return TOKEN_EOF when reaching the end of the input stream.
+  private Token nextTokenInternal() throws Exception
+  {
+	 
+	  
+	  Kind k;
+    int c = this.fstream.read();
+    if (-1 == c)
+    	
+      // The value for "lineNum" is now "null",
+      // you should modify this to an appropriate
+      // line number for the "EOF" token.
+      return new Token(Kind.TOKEN_EOF, linenum);
+
+    // skip all kinds of "blanks" 
+    while ('\t' == c || '\n' == c) {
+    	if('\n'==c)
+    		linenum++;
+      c = this.fstream.read();
+      //System.out.println((char)c);
+    }
+    if (-1 == c)
+    	 
+      return new Token(Kind.TOKEN_EOF, linenum);
+
+    switch (c) {
+    case ' ':
+    {
+    	return printTokenforspace(c);
+    		 
+    }
+    case '+':
+    {
+    	cmp=printTokenforspace(c);
+    	if(cmp==null)
+    		return new Token(Kind.TOKEN_ADD,linenum);
+    	else
+    		return behind;
+    } 
+    case '=':
+    {
+    	cmp=printTokenforspace(c);
+    	if(cmp==null)
+    		return new Token(Kind.TOKEN_ASSIGN,linenum);
+    	else
+    		return behind;
+    }
+    case ',':
+    {
+    	cmp=printTokenforspace(c);
+    	if(cmp==null)
+    		return new Token(Kind.TOKEN_COMMER,linenum);
+    	else
+    		return behind;
+    }
+    case '.':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_DOT,linenum);
+        	else
+        		return behind;
+    	}
+    case '{':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_LBRACE,linenum);
+        	else
+        		return behind;
+    	}
+    case '[':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_LBRACK,linenum);
+        	else
+        		return behind;
+    	}
+    case '(':
+    {
+    	cmp=printTokenforspace(c);
+    	if(cmp==null)
+    		return new Token(Kind.TOKEN_LPAREN,linenum);
+    	else
+    		return behind;
+    }
+    case '<':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_LT,linenum);
+        	else
+        		return behind;
+    	}
+    case '!':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_NOT,linenum);
+        	else
+        		return behind;
+    	}
+    case '}':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_RBRACE,linenum);
+        	else
+        		return behind;
+    	}
+    case ']':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_RBRACK,linenum);
+        	else
+        		return behind;
+    	}
+    case ')':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_RPAREN,linenum);
+        	else
+        		return behind;
+    	}
+    case ';':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_SEMI,linenum);
+        	else
+        		return behind;
+    	}
+    case '-':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_SUB,linenum);
+        	else
+        		return behind;
+    	}
+    case '*':
+    	{
+    		cmp=printTokenforspace(c);
+        	if(cmp==null)
+        		return new Token(Kind.TOKEN_TIMES,linenum);
+        	else
+        		return behind;
+    	}
+    	
+    default:
+      // Lab 1, exercise 2: supply missing code to
+      // lex other kinds of tokens.
+      // Hint: think carefully about the basic
+      // data structure and algorithms. The code
+      // is not that much and may be less than 50 lines. If you
+      // find you are writing a lot of code, you
+      // are on the wrong way.
+    	
+     // new Todo();
+    	s+=(char)(c);
+    	 //System.out.println(s);
+    	return nextTokenInternal();
+    	
+    }
+	
+  }
+  
+  public Token printTokenforspace(int c) throws IOException
+  {
+		  if(s!="")
+		  {
+			  Token token=new Token();
+			  Kind k=token.getkey(s);
+  			  behind=new Token(k,linenum);
+  			  s="";
+  			  if(c!=32)
+  			  fstream.unread(c);
+  			  return behind;
+  			  
+  	} 
+	  return null;
+	  
+  }
+
+
+public Token nextToken()
+  {
+    Token t = null;
+
+    try {
+      while(t==null)
+    	  t=this.nextTokenInternal();
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+    if (dump)
+      System.out.println(t.toString());
+    return t;
+  }
+
+
+}
---
+++ tiger/src/lexer/Token.java
@@ -0,0 +1,214 @@
+package lexer;
+
+public class Token
+{
+	static String  key[]={
+			"+",
+		    "&&",
+		     "=",
+		    "boolean",
+		    "class",
+		    ",",
+		    ".",
+		    "else",
+		     "EOF",
+		     "extends",
+		    "false",
+		    "~~~~~",
+		    "if",
+		    "int",
+		     "{",
+		    "[",
+		    "length",
+		    "(",
+		     "<",
+		     "main",
+		     "new",
+		    "!",
+		    "~~~~~~~~",
+		    "out",
+		     "println",
+		     "public",
+		    "}",
+		     "]",
+		    "return",
+		     ")",
+		     ";",
+		    "static",
+		     "String",
+		     "-",
+		     "System",
+		     "this",
+		     "*",
+		     "true",
+		     "void",
+		     "while"
+			};
+   String Kind1[] =
+			{
+	    "TOKEN_ADD", // "+"
+	    "TOKEN_AND", // "&&"
+	    "TOKEN_ASSIGN", // "="
+	    "TOKEN_BOOLEAN", // "boolean"
+	    "TOKEN_CLASS", // "class"
+	    "TOKEN_COMMER", // ","
+	    "TOKEN_DOT", // "."
+	    "TOKEN_ELSE", // "else"
+	    "TOKEN_EOF", // EOF
+	    "TOKEN_EXTENDS", // "extends"
+	    "TOKEN_FALSE", // "false"
+	    "TOKEN_ID", // Identifier
+	    "TOKEN_IF", // "if"
+	    "TOKEN_INT", // "int"
+	    "TOKEN_LBRACE", // "{"
+	    "TOKEN_LBRACK", // "["
+	    "TOKEN_LENGTH", // "length"
+	    "TOKEN_LPAREN", // "("
+	    "TOKEN_LT", // "<"
+	    "TOKEN_MAIN", // "main"
+	    "TOKEN_NEW", // "new"
+	    "TOKEN_NOT", // "!"
+	    "TOKEN_NUM", // IntegerLiteral
+	    "TOKEN_OUT", // "out"
+	    "TOKEN_PRINTLN", // "println"
+	    "TOKEN_PUBLIC", // "public"
+	    "TOKEN_RBRACE", // "}"
+	    "TOKEN_RBRACK", // "]"
+	    "TOKEN_RETURN", // "return"
+	    "TOKEN_RPAREN", // ")"
+	    "TOKEN_SEMI", // ";"
+	    "TOKEN_STATIC", // "static"
+	    "TOKEN_STRING", // "String"
+	    "TOKEN_SUB", // "-"
+	    "TOKEN_SYSTEM", // "System"
+	    "TOKEN_THIS", // "this"
+	    "TOKEN_TIMES", // "*"
+	    "TOKEN_TRUE", // "true"
+	    "TOKEN_VOID", // "void"
+	    "TOKEN_WHILE"// "while"
+			};
+  // Lab 1, exercise 1: read the MiniJava specification
+  // carefully, and answer these two questions:
+  //   1. whether or not one should add other token kinds?
+  //   2. which tokens come with an extra "lexeme", and
+  //      which don't?
+  // It's highly recommended that these token names are
+  // alphabetically ordered, if you add new ones.
+  public  enum Kind {
+    TOKEN_ADD, // "+"
+    TOKEN_AND, // "&&"
+    TOKEN_ASSIGN, // "="
+    TOKEN_BOOLEAN, // "boolean"
+    TOKEN_CLASS, // "class"
+    TOKEN_COMMER, // ","
+    TOKEN_DOT, // "."
+    TOKEN_ELSE, // "else"
+    TOKEN_EOF, // EOF
+    TOKEN_EXTENDS, // "extends"
+    TOKEN_FALSE, // "false"
+    TOKEN_ID, // Identifier
+    TOKEN_IF, // "if"
+    TOKEN_INT, // "int"
+    TOKEN_LBRACE, // "{"
+    TOKEN_LBRACK, // "["
+    TOKEN_LENGTH, // "length"
+    TOKEN_LPAREN, // "("
+    TOKEN_LT, // "<"
+    TOKEN_MAIN, // "main"
+    TOKEN_NEW, // "new"
+    TOKEN_NOT, // "!"
+    TOKEN_NUM, // IntegerLiteral
+    // "out" is not a Java key word, but we treat it as
+    // a MiniJava keyword, which will make the
+    // compilation a little easier. Similar cases apply
+    // for "println", "System" and "String".
+    TOKEN_OUT, // "out"
+    TOKEN_PRINTLN, // "println"
+    TOKEN_PUBLIC, // "public"
+    TOKEN_RBRACE, // "}"
+    TOKEN_RBRACK, // "]"
+    TOKEN_RETURN, // "return"
+    TOKEN_RPAREN, // ")"
+    TOKEN_SEMI, // ";"
+    TOKEN_STATIC, // "static"
+    TOKEN_STRING, // "String"
+    TOKEN_SUB, // "-"
+    TOKEN_SYSTEM, // "System"
+    TOKEN_THIS, // "this"
+    TOKEN_TIMES, // "*"
+    TOKEN_TRUE, // "true"
+    TOKEN_VOID, // "void"
+    TOKEN_WHILE;
+    // "while"
+    
+    public static Kind valueOf(int ordinal) {
+        if (ordinal < 0 || ordinal >= values().length) {
+            throw new IndexOutOfBoundsException("Invalid ordinal");
+        }
+        return values()[ordinal];
+    }
+    
+ 
+    
+  }
+
+  public Kind kind; // kind of the token
+  public String lexeme; // extra lexeme for this token, if any
+  public Integer lineNum; // on which line of the source file this token appears
+
+  // Some tokens don't come with lexeme but 
+  // others do.
+  public Token(Kind kind, Integer lineNum)
+  {
+    this.kind = kind;
+    this.lineNum = lineNum;
+  }
+
+  public Token(Kind kind, Integer lineNum, String lexeme)
+  {
+    this(kind, lineNum);
+    this.lexeme = lexeme;
+  }
+  public Token()
+  {
+	  ;
+  }
+
+  @Override
+  public String toString()
+  {
+    String s;
+
+    // to check that the "lineNum" field has been properly set.
+    if (this.lineNum == null)
+      new util.Todo();
+    	
+
+    s = ": " + ((this.lexeme == null) ? "<NONE>" : this.lexeme) + " : at line "
+        + this.lineNum.toString();
+    return this.kind.toString() + s;
+  }
+ 
+  public Kind getkey(String s)
+  {
+	  int i;
+	  if(isNum(s))
+		  return Kind.TOKEN_NUM;
+	  for(i=0;i<key.length;i++)
+	  {
+		  if(s.equals(key[i]))
+			  return Kind.valueOf(i);
+			  
+	  }
+	  
+	  return Kind.TOKEN_ID;
+  }
+
+  public static boolean isNum(String str){
+	   
+	      int chr=str.charAt(0);
+	      if(chr<48 || chr>57)
+	         return false;
+	     return true;
+	}
+}
