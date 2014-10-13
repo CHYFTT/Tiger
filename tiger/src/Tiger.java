523457788398acbea4b161f3cdde478089b6baa6
QingchuanLiu <416521842@qq.com>
2014-10-13 19:20:18 +0800

    format!

--- tiger/src/Tiger.java
+++ tiger/src/Tiger.java
@@ -9,79 +9,66 @@ import parser.Parser;
 import control.CommandLine;
 import control.Control;
 
-public class Tiger
-{
-  public static void main(String[] args)
-  {
-    InputStream fstream;
-    Parser parser;
-    PushbackInputStream f;
+public class Tiger {
+	public static void main(String[] args) {
+		InputStream fstream;
+		Parser parser;
+		PushbackInputStream f;
 
-  
+		// ///////////////////////////////////////////////////////
+		// handle command line arguments
+		CommandLine cmd = new CommandLine();
+		String fname = cmd.scan(args);
 
-    // ///////////////////////////////////////////////////////
-    // handle command line arguments
-    CommandLine cmd = new CommandLine();
-    String fname = cmd.scan(args);
+		// /////////////////////////////////////////////
+		// the straight-line interpreter (and compiler)
+		/*
+		 * switch (Control.ConSlp.action){ case NONE: System.exit(0); break;
+		 * default: slp.Main slpmain = new slp.Main(); if (Control.ConSlp.div) {
+		 * slpmain.doit(slp.Samples.dividebyzero); System.exit(0); }
+		 * slpmain.doit(slp.Samples.prog); System.exit(0); }
+		 */
 
-    // /////////////////////////////////////////////
-    // the straight-line interpreter (and compiler)   
-    /*
-    switch (Control.ConSlp.action){
-    case NONE:
-      System.exit(0);
-      break;
-    default:
-      slp.Main slpmain = new slp.Main();
-      if (Control.ConSlp.div) {
-        slpmain.doit(slp.Samples.dividebyzero);
-        System.exit(0);
-      }
-      slpmain.doit(slp.Samples.prog);
-      System.exit(0);
-    }
-    */
+		if (fname == null) {
+			cmd.usage();
+			return;
+		}
 
-    if (fname == null) {
-      cmd.usage();
-      return;
-    }
+		// /////////////////////////////////////////////////////
+		// it would be helpful to be able to test the lexer
+		// independently.
+		if (Control.ConLexer.test) {
+			System.out.println("Testing the lexer. All tokens:");
+			try {
+				fstream = new BufferedInputStream(new FileInputStream(fname));
+				f = new PushbackInputStream(fstream);
+				Lexer lexer = new Lexer(fname, f);
+				Token token = lexer.nextToken();
+				while (token.kind != Token.Kind.TOKEN_EOF) {
+					System.out.println(token.toString());
+					token = lexer.nextToken();
+				}
+				fstream.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.exit(1);
+		}
 
-    // /////////////////////////////////////////////////////
-    // it would be helpful to be able to test the lexer
-    // independently.
-    if (Control.ConLexer.test) {
-        System.out.println("Testing the lexer. All tokens:");
-        try {
-          fstream = new BufferedInputStream(new FileInputStream(fname));
-          f=new PushbackInputStream(fstream);
-          Lexer lexer = new Lexer(fname, f);
-          Token token = lexer.nextToken();
-          while (token.kind != Token.Kind.TOKEN_EOF) {
-            System.out.println(token.toString());
-            token = lexer.nextToken();
-          }
-          fstream.close();
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-        System.exit(1);
-      }
+		// /////////////////////////////////////////////////////////
+		// normal compilation phases.
+		try {
+			fstream = new BufferedInputStream(new FileInputStream(fname));
+			f = new PushbackInputStream(fstream);
+			parser = new Parser(fname, f);
 
-    // /////////////////////////////////////////////////////////
-    // normal compilation phases.
-    try {
-      fstream = new BufferedInputStream(new FileInputStream(fname));
-      f=new PushbackInputStream(fstream);
-      parser = new Parser(fname,f);
+			parser.parse();
 
-      parser.parse();
-
-      fstream.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-    return;
-  }
+			fstream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return;
+	}
 }
